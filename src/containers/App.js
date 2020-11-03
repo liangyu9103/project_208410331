2df99ac595262a61435d6f8f3928cb1f3266083e
liangyu9103 <liangyu9103@gmail.com>
2020-11-03 19:12:04 +0800

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import React,{Component} from 'react';
+import CardList31 from '../CardList31';
+import { robots } from '../robots';
+import SearchBox31 from '../SearchBox31';
+import Scroll31 from '../Scroll31';
+// import '';
+
+class App extends Component   {
+    constructor() {
+    super();
+    this.state = {
+    robots: [],  
+    robots: robots,
+    searchfield: ''
+    };
+}
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));  
+}
+
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+};  
+render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+    return robot.name  
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+
+    return (
+        <div className='tc'>
+          <h1>DEMO</h1>
+      <SearchBox31 searchChange={this.onSearchChange} />
+      <Scroll31>
+      <CardList31 robots={filteredRobots} />;
+      </Scroll31>
+        </div> 
+    );
+   }
+  }
+};
+
+export default App; 
---
+++ src/robots.js
@@ -0,0 +1,53 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Leanne Graham31',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+},
+{   id: 2,
+    name: 'Ervin Howell31',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'  
+},  
+{   id: 3,    
+    name: 'Clementine Bauch31',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'  
+},  
+{   id: 4,
+    name: 'Patricia Lebsack31',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'  
+},  
+{   id: 5,
+    name: 'Chelsey Dietrich31',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'  
+},  
+{    
+    id: 6,
+    name: 'Mrs. Dennis Schulist31',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'  
+},  
+{   id: 7,
+    name: 'Kurtis Weissnat31',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+},
+{   id: 8,
+    name: 'Nicholas Runolfsdottir V31',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+},
+{   id: 9,
+    name: 'Glenna Reichert31',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+},
+{   id: 10,
+    name: 'Clementina DuBuque31',
+    username: 'Moriah.Stanton', 
+    email: 'Rey.Padberg@karina.biz'  
+}];
